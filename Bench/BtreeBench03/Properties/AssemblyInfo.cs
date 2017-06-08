e845c40cc1b780c7383016b8f0f460dfb03575b5
kaosborn <kaosborn@users.noreply.github.com>
2017-06-08 00:27:06 -0700

    move BtreeExample01 to BtreeBench03

--- Bench/BtreeBench03/Properties/AssemblyInfo.cs
+++ Bench/BtreeBench03/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle ("BtreeExample01")]
+[assembly: AssemblyTitle ("BtreeBench03")]
 [assembly: AssemblyDescription ("")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
-[assembly: AssemblyProduct ("BtreeExample01")]
+[assembly: AssemblyProduct ("BtreeBench03")]
 [assembly: AssemblyCopyright ("Copyright ©  2017")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible (false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid ("9d913856-8058-4708-b63a-6eefce22d6a3")]
+[assembly: Guid ("b2e1c469-4a85-4f63-8d62-d43443a8b294")]
 
 // Version information for an assembly consists of the following four values:
 //
