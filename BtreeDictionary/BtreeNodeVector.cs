07dcd15fa310c02336714b493401265df706f6a1
kaosborn <kaosborn@users.noreply.github.com>
2017-06-15 04:44:29 -0700

    move helper methods to NodeVector continued

--- BtreeDictionary/BtreeNodeVector.cs
+++ BtreeDictionary/BtreeNodeVector.cs
@@ -16,16 +16,18 @@ namespace Kaos.Collections
     {
         /// <summary>Stack trace from root to leaf of a key/value pair.</summary>
         /// <remarks>
-        /// Performs search function for key.
-        /// Provides directions to existing key or insertion point for non-existing key.
+        /// Provides directions to existing key or insertion point for non-existing key
+        /// along with various helper methods.
         /// </remarks>
         /// <typeparam name="TKey">Key type.</typeparam>
         /// <typeparam name="TValue">Value type.</typeparam>
-        private partial class NodeVector
+        private class NodeVector
         {
             private BtreeDictionary<TKey, TValue> owner;
             private List<int> indexStack;
             private List<Node> nodeStack;
+
+            #region Constructors
 
             /// <summary>Perform search and store each level of path on the stack.</summary>
             /// <param name="tree">Tree to search.</param>
@@ -58,6 +60,8 @@ namespace Kaos.Collections
                 }
             }
 
+            #endregion
+
             #region Properties
 
             public bool IsFound
@@ -88,6 +92,8 @@ namespace Kaos.Collections
             }
 
             #endregion
+
+            #region Methods
 
             /// <summary>
             /// Get the node to the immediate left of the node specified by NodeVector.
@@ -196,6 +202,276 @@ namespace Kaos.Collections
 
                 return node;
             }
+
+
+            /// <summary>Insert element at preseeked path.</summary>
+            public void Insert (TKey key, TValue value)
+            {
+                var leaf = (Leaf) TopNode;
+                int pathIndex = TopNodeIndex;
+
+                if (leaf.KeyCount < owner.maxKeyCount)
+                {
+                    leaf.Insert (pathIndex, key, value);
+                    ++owner.Count;
+                    return;
+                }
+
+                // Leaf is full so right split a new leaf.
+                var newLeaf = new Leaf (leaf, owner.maxKeyCount);
+
+                if (newLeaf.RightLeaf == null && pathIndex == leaf.KeyCount)
+                    newLeaf.Add (key, value);
+                else
+                {
+                    int splitIndex = leaf.KeyCount / 2 + 1;
+
+                    if (pathIndex < splitIndex)
+                    {
+                        // Left-side insert: Copy right side to the split leaf.
+                        newLeaf.Add (leaf, splitIndex - 1, leaf.KeyCount);
+                        leaf.Truncate (splitIndex - 1);
+                        leaf.Insert (pathIndex, key, value);
+                    }
+                    else
+                    {
+                        // Right-side insert: Copy split leaf parts and new key.
+                        newLeaf.Add (leaf, splitIndex, pathIndex);
+                        newLeaf.Add (key, value);
+                        newLeaf.Add (leaf, pathIndex, leaf.KeyCount);
+                        leaf.Truncate (splitIndex);
+                    }
+                }
+
+                // Promote anchor of split leaf.
+                ++owner.Count;
+                Promote (newLeaf.GetKey (0), (Node) newLeaf, newLeaf.RightLeaf == null);
+            }
+
+
+            // Leaf or branch has been split so insert the new anchor into a branch.
+            private void Promote (TKey key, Node newNode, bool isAppend)
+            {
+                for (;;)
+                {
+                    if (Height == 1)
+                    {
+                        // Graft new root.
+                        Debug.Assert (owner.root == TopNode);
+                        owner.root = new Branch (TopNode, owner.maxKeyCount);
+                        owner.root.Add (key, newNode);
+                        break;
+                    }
+
+                    Pop();
+                    var branch = (Branch) TopNode;
+                    int branchIndex = TopNodeIndex;
+
+                    if (branch.KeyCount < owner.maxKeyCount)
+                    {
+                        // Typical case where branch has room.
+                        branch.InsertKey (branchIndex, key);
+                        branch.Insert (branchIndex + 1, newNode);
+                        break;
+                    }
+
+                    // Branch is full so right split a new branch.
+                    var newBranch = new Branch (branch, owner.maxKeyCount);
+                    int splitIndex = isAppend ? branch.KeyCount - 2 : (branch.KeyCount + 1) / 2;
+
+                    if (branchIndex < splitIndex)
+                    {
+                        // Split with left-side insert.
+                        for (int ix = splitIndex; ; ++ix)
+                        {
+                            if (ix >= branch.KeyCount)
+                            {
+                                newBranch.Add (branch.GetChild (ix));
+                                break;
+                            }
+                            newBranch.Add (branch.GetKey (ix), branch.GetChild (ix));
+                        }
+
+                        TKey newPromotion = branch.GetKey (splitIndex - 1);
+                        branch.Truncate (splitIndex - 1);
+                        branch.InsertKey (branchIndex, key);
+                        branch.Insert (branchIndex + 1, newNode);
+                        key = newPromotion;
+                    }
+                    else
+                    {
+                        // Split branch with right-side insert (or cascade promote).
+                        int leftIndex = splitIndex;
+
+                        if (branchIndex > splitIndex)
+                        {
+                            for (;;)
+                            {
+                                ++leftIndex;
+                                newBranch.Add (branch.GetChild (leftIndex));
+                                if (leftIndex >= branchIndex)
+                                    break;
+                                newBranch.AddKey (branch.GetKey (leftIndex));
+                            }
+                            newBranch.AddKey (key);
+                            key = branch.GetKey (splitIndex);
+                        }
+
+                        newBranch.Add (newNode);
+
+                        while (leftIndex < branch.KeyCount)
+                        {
+                            newBranch.AddKey (branch.GetKey (leftIndex));
+                            ++leftIndex;
+                            newBranch.Add (branch.GetChild (leftIndex));
+                        }
+
+                        branch.Truncate (splitIndex);
+                    }
+
+                    newNode = newBranch;
+                }
+            }
+
+            /// <summary>Delete specified key-vaue pair.</summary>
+            public void Delete()
+            {
+                int leafIndex = TopNodeIndex;
+                var leaf = (Leaf) TopNode;
+
+                leaf.Remove (leafIndex);
+                --owner.Count;
+
+                if (leafIndex == 0)
+                    if (leaf.KeyCount != 0)
+                        SetPivot (TopNode.GetKey (0));
+                    else
+                    {
+                        Debug.Assert (leaf.RightLeaf == null, "only the rightmost leaf should ever be emptied");
+
+                        // Leaf is empty.  Prune it unless it is the only leaf in the tree.
+                        var leftLeaf = (Leaf) GetLeftNode();
+                        if (leftLeaf != null)
+                        {
+                            leftLeaf.RightLeaf = leaf.RightLeaf;
+                            Demote();
+                        }
+
+                        return;
+                    }
+
+                // Leaf underflow?
+                if (leaf.KeyCount < owner.maxKeyCount / 2)
+                {
+                    Leaf rightLeaf = leaf.RightLeaf;
+                    if (rightLeaf != null)
+                        if (leaf.KeyCount + rightLeaf.KeyCount > owner.maxKeyCount)
+                        {
+                            // Balance leaves by shifting pairs from right leaf.
+                            int shifts = owner.maxKeyCount - (leaf.KeyCount + rightLeaf.KeyCount - 1) / 2;
+                            leaf.Add (rightLeaf, 0, shifts);
+                            rightLeaf.Remove (0, shifts);
+                            TraverseRight();
+                            SetPivot (TopNode.GetKey (0));
+                        }
+                        else
+                        {
+                            // Coalesce right leaf to current leaf and prune right leaf.
+                            leaf.Add (rightLeaf, 0, rightLeaf.KeyCount);
+                            leaf.RightLeaf = rightLeaf.RightLeaf;
+                            TraverseRight();
+                            Demote();
+                        }
+                }
+            }
+
+
+            // Leaf has been emptied so non-lazy delete its pivot.
+            private void Demote()
+            {
+                for (;;)
+                {
+                    Debug.Assert (Height > 0);
+                    Pop();
+
+                    var branch = (Branch) TopNode;
+                    if (TopNodeIndex == 0)
+                    {
+                        if (branch.KeyCount == 0)
+                            // Cascade when rightmost branch is empty.
+                            continue;
+
+                        // Rotate pivot for first child.
+                        TKey pivot = branch.GetKey (0);
+                        branch.RemoveKey (0);
+                        branch.RemoveChild (0);
+                        SetPivot (pivot);
+                    }
+                    else
+                    {
+                        // Typical branch pivot delete.
+                        branch.RemoveKey (TopNodeIndex - 1);
+                        branch.RemoveChild (TopNodeIndex);
+                    }
+
+                    var right = (Branch) TraverseRight();
+                    if (right == null)
+                    {
+                        if (branch == owner.root && branch.KeyCount == 0)
+                        {
+                            // Prune the empty root.
+                            var newRoot = branch.FirstChild as Branch;
+                            if (newRoot != null)
+                                owner.root = newRoot;
+                        }
+                        return;
+                    }
+
+                    if (branch.KeyCount + right.KeyCount < owner.maxKeyCount)
+                    {
+                        // Coalesce left: move pivot and right sibling nodes.
+                        branch.AddKey (GetPivot());
+
+                        for (int ix = 0; ; ++ix)
+                        {
+                            branch.Add (right.GetChild (ix));
+                            if (ix >= right.KeyCount)
+                                break;
+                            branch.AddKey (right.GetKey (ix));
+                        }
+
+                        // Cascade demotion.
+                        continue;
+                    }
+
+                    // Branch underflow?
+                    if (branch.KeyCount < owner.maxKeyCount / 2)
+                    {
+                        int shifts = (right.KeyCount - branch.KeyCount) / 2 - 1;
+
+                        // Balance branches to keep ratio.  Rotate thru the pivot.
+                        branch.AddKey (GetPivot());
+
+                        // Shift pairs from right sibling.
+                        for (int rightIndex = 0; ; ++rightIndex)
+                        {
+                            branch.Add (right.GetChild (rightIndex));
+
+                            if (rightIndex >= shifts)
+                                break;
+
+                            branch.AddKey (right.GetKey (rightIndex));
+                        }
+
+                        SetPivot (right.GetKey (shifts));
+                        right.Remove (0, shifts + 1);
+                    }
+
+                    return;
+                }
+            }
+
+            #endregion
 
 #if DEBUG
             public bool IsFirstChild
