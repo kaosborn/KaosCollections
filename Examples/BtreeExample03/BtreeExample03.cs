6573eb19d889bd4f7f8f4b54968304f9e531c207
kaosborn <kaosborn@users.noreply.github.com>
2017-06-28 00:34:09 -0700

    add output, explicit types

--- Examples/BtreeExample03/BtreeExample03.cs
+++ Examples/BtreeExample03/BtreeExample03.cs
@@ -5,6 +5,7 @@
 
 using System;
 using System.Linq;
+using System.Collections.Generic;
 using Kaos.Collections;
 
 namespace ExampleApp
@@ -13,7 +14,7 @@ namespace ExampleApp
     {
         static void Main()
         {
-            var towns = new BtreeDictionary<string, int>();
+            var towns = new BtreeDictionary<string,int>();
 
             // Load sample data.
             towns.Add ("Albany", 43600);
@@ -40,19 +41,18 @@ namespace ExampleApp
             double avg = towns.Average (x => x.Value);
             Console.WriteLine ("Average population of all towns = {0:f0}", avg);
 
-
             // Lambda expression
-            var r1 =  towns.Where (t => t.Key.CompareTo ("E") < 0);
+            IEnumerable<KeyValuePair<string,int>> r1 = towns.Where (t => t.Key.CompareTo ("E") < 0);
 
             Console.WriteLine ("\nTowns A-D:");
-            foreach (var e in r1)
+            foreach (KeyValuePair<string,int> e in r1)
                 Console.WriteLine (e.Key);
 
             // LINQ range: O(n)
-            var r2 =  towns.SkipWhile (t => t.Key.CompareTo ("E") < 0).TakeWhile (t => t.Key.CompareTo ("J") < 0);
+            IEnumerable<KeyValuePair<string,int>> r2 = towns.SkipWhile (t => t.Key.CompareTo ("E") < 0).TakeWhile (t => t.Key.CompareTo ("J") < 0);
 
             Console.WriteLine ("\nTowns E-G:");
-            foreach (var e in r2)
+            foreach (KeyValuePair<string,int> e in r2)
                 Console.WriteLine (e.Key);
 
             //
@@ -61,17 +61,17 @@ namespace ExampleApp
             //
 
             // BtreeDictionary range operator: O(log n)
-            var r3 = towns.BetweenKeys ("K", "M");
+            IEnumerable<KeyValuePair<string,int>> r3 = towns.BetweenKeys ("K", "M");
 
             Console.WriteLine ("\nTowns K-L:");
-            foreach (var town in r3)
+            foreach (KeyValuePair<string,int> town in r3)
                 Console.WriteLine (town.Key);
 
             // BtreeDictionary range operator without upper limit: O(log n)
-            var r4 = towns.SkipUntilKey ("M");
+            IEnumerable<KeyValuePair<string,int>> r4 = towns.SkipUntilKey ("M");
 
             Console.WriteLine ("\nTowns M-R:");
-            foreach (var town in r4)
+            foreach (KeyValuePair<string,int> town in r4)
                 // This avoids the issue in the last example where a town named "M" would be included.
                 if (town.Key.CompareTo ("S") >= 0)
                     break;
@@ -79,11 +79,44 @@ namespace ExampleApp
                     Console.WriteLine (town.Key);
 
             // BtreeDictionary range operator without upper limit: O(log n)
-            var r5 = towns.SkipUntilKey ("T");
+            IEnumerable<KeyValuePair<string,int>> r5 = towns.SkipUntilKey ("T");
 
             Console.WriteLine ("\nTowns T-Z:");
-            foreach (var town in r5)
+            foreach (KeyValuePair<string,int> town in r5)
                 Console.WriteLine (town.Key);
         }
+
+        /* Output:
+
+        Average population of all towns = 13447
+
+        Towns A-D:
+        Albany
+        Bandon
+        Corvallis
+        Damascus
+
+        Towns E-G:
+        Elkton
+        Florence
+        Glide
+
+        Towns K-L:
+        Lebanon
+        Lookingglass
+
+        Towns M-R:
+        Medford
+        Powers
+        Riddle
+        Roseburg
+
+        Towns T-Z:
+        Talent
+        Umatilla
+        Winston
+        Yamhill
+
+         */
     }
 }
