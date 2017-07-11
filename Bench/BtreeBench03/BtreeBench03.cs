b6fdfc38bc4f30cd5e6ec01faab93e7cbf266093
kaosborn <kaosborn@users.noreply.github.com>
2017-07-10 23:21:18 -0700

    add BtreeBench03 project

---
+++ Bench/BtreeBench03/BtreeBench03.cs
@@ -0,0 +1,30 @@
+﻿//
+// Program: BtreeBench03.cs
+// Purpose: Benchmark BtreeDictionary fill percent.
+//
+// Usage notes:
+// • Run in Debug build for diagnostics.
+//
+
+using System;
+using System.Reflection;
+using Kaos.Collections;
+[assembly: AssemblyVersion ("0.1.0.0")]
+
+namespace BenchApp
+{
+    public class BtreeBench03
+    {
+        static int reps = 50000000;
+        static BtreeDictionary<Guid,int> tree = new BtreeDictionary<Guid,int> (128);
+
+        static void Main (string[] args)
+        {
+            for (int ii = 0; ii < reps; ++ii)
+                tree.Add (Guid.NewGuid(), ii);
+
+            Console.WriteLine (reps);
+            Console.WriteLine (tree.GetTreeStatsText());
+        }
+    }
+}
