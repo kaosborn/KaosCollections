80f99342ab65d8e0a67a95f6dc4bb8128bf587da
kaosborn <kaosborn@users.noreply.github.com>
2017-07-11 23:31:58 -0700

    add RankedDictionary project

---
+++ RankedDictionary/BtreeDiagnostics.cs
@@ -0,0 +1,205 @@
+﻿//
+// Library: KaosCollections
+// File:    BtreeDiagnostics.cs
+// Purpose: Define BtreeDictionary API for Debug build only.
+//
+// Copyright © 2009-2017 Kasey Osborn (github.com/kaosborn)
+// MIT License - Use and redistribute freely
+//
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kaos.Collections
+{
+#if DEBUG
+    public partial class BtreeDictionary<TKey,TValue>
+    {
+        // Telemetry counters:
+
+        /// <summary>Maximum number of keys that the existing branches can hold.</summary>
+        public int BranchSlotCount { get; private set; }
+        /// <summary>Number of keys contained in the branches.</summary>
+        public int BranchSlotsUsed { get; private set; }
+        /// <summary>Maximum number of keys that the existing leaves can hold.</summary>
+        public int LeafSlotCount { get; private set; }
+        /// <summary>Number of keys contained in the leaves.</summary>
+        public int LeafSlotsUsed { get; private set; }
+
+
+        /// <summary>
+        /// Perform diagnostics check for data structure sanity errors. Since this is an
+        /// in-memory managed structure, any errors would indicate a bug. Also performs space
+        /// complexity diagnostics to ensure that all non-rightmost nodes maintain 50% fill.
+        /// </summary>
+        public void SanityCheck()
+        {
+            BranchSlotCount = 0;
+            BranchSlotsUsed = 0;
+            LeafSlotCount = 0;
+            LeafSlotsUsed = 0;
+
+            Leaf lastLeaf;
+            if (root is Branch)
+                lastLeaf = CheckBranch ((Branch) root, 1, GetHeight(), true, default (TKey), null);
+            else
+                lastLeaf = CheckLeaf ((Leaf) root, true, default (TKey), null);
+
+            if (lastLeaf.RightLeaf != null)
+                throw new InvalidOperationException ("Last leaf has invalid RightLeaf");
+
+            if (Count != LeafSlotsUsed)
+                throw new InvalidOperationException ("Mismatched Count=" + Count + ", expected=" + LeafSlotsUsed);
+        }
+
+
+        /// <summary>Maximum number of children of a branch.</summary>
+        /// <returns>Maximum number of children of a branch.</returns>
+        public int GetOrder()
+        { return maxKeyCount + 1; }
+
+
+        /// <summary>Return the number of levels in the tree.</summary>
+        /// <returns>Number of levels in the tree.</returns>
+        public int GetHeight()
+        {
+            int level = 1;
+            for (Node node = root; node is Branch; node = ((Branch) node).Child0)
+                ++level;
+            return level;
+        }
+
+
+        private Leaf CheckBranch
+        (
+            Branch branch,
+            int level, int height,
+            bool isRightmost,
+            TKey anchor,  // ignored when isRightmost true
+            Leaf visited
+        )
+        {
+            BranchSlotCount += maxKeyCount;
+            BranchSlotsUsed += branch.KeyCount;
+
+            if (! isRightmost && (branch.KeyCount + 1) < maxKeyCount / 2)
+                throw new InvalidOperationException ("Branch underfilled");
+
+            if (branch.ChildCount != branch.KeyCount + 1)
+                throw new InvalidOperationException ("Branch mismatched ChildCount, KeyCount");
+
+            for (int i = 0; i < branch.ChildCount; ++i)
+            {
+                TKey anchor0 = i == 0 ? anchor : branch.GetKey (i - 1);
+                bool isRightmost0 = isRightmost && i < branch.ChildCount;
+                if (i < branch.KeyCount - 1)
+                    if (branch.GetKey (i).CompareTo (branch.GetKey (i + 1)) >= 0)
+                        throw new InvalidOperationException ("Branch keys not ascending");
+
+                if (level + 1 < height)
+                    visited = CheckBranch ((Branch) branch.GetChild (i), level+1, height, isRightmost0, anchor0, visited);
+                else
+                    visited = CheckLeaf ((Leaf) branch.GetChild (i), isRightmost0, anchor0, visited);
+            }
+            return visited;
+        }
+
+
+        private Leaf CheckLeaf (Leaf leaf, bool isRightmost, TKey anchor, Leaf visited)
+        {
+            LeafSlotCount += maxKeyCount;
+            LeafSlotsUsed += leaf.KeyCount;
+
+            if (leaf.RightLeaf != null && leaf.KeyCount < (maxKeyCount + 1) / 2)
+                throw new InvalidOperationException ("Leaf underfilled");
+
+            if (! anchor.Equals (default (TKey)) && ! anchor.Equals (leaf.Key0))
+                throw new InvalidOperationException ("Leaf has wrong anchor");
+
+            for (int i = 0; i < leaf.KeyCount; ++i)
+                if (i < leaf.KeyCount - 1 && leaf.GetKey (i).CompareTo (leaf.GetKey (i + 1)) >= 0)
+                    throw new InvalidOperationException ("Leaf keys not ascending");
+
+            if (visited == null)
+            {
+                if (! anchor.Equals (default (TKey)))
+                    throw new InvalidOperationException ("Inconsistent visited, anchor");
+            }
+            else
+                if (visited.RightLeaf != leaf)
+                    throw new InvalidOperationException ("Leaf has bad RightLeaf");
+
+            return leaf;
+        }
+
+
+        /// <summary>Return telemetry summary.</summary>
+        /// <returns>Telemetry summary.</returns>
+        public string GetTreeStatsText()
+        {
+            SanityCheck();
+            string result = "--- height = " + GetHeight();
+
+            if (BranchSlotCount != 0)
+                result += ", branch fill = " + BranchSlotsUsed * 100 / BranchSlotCount + "%";
+
+            return result + ", leaf fill = " + LeafSlotsUsed * 100 / LeafSlotCount + "%";
+        }
+
+
+        /// <summary>Generate contents of tree by level (breadth first).</summary>
+        /// <returns>Text lines where each line is a level of the tree.</returns>
+        public IEnumerable<string> GenerateTreeText()
+        {
+            int level = 0;
+            Node leftmost;
+            var sb = new StringBuilder();
+
+            for (;;)
+            {
+                var branchPath = new NodeVector (this, level);
+                leftmost = branchPath.TopNode;
+                if (leftmost is Leaf)
+                    break;
+
+                var branch = (Branch) leftmost;
+
+                sb.Append ('B');
+                sb.Append (level);
+                sb.Append (": ");
+                for (;;)
+                {
+                    branch.Append (sb);
+                    branch = (Branch) branchPath.TraverseRight();
+
+                    if (branch == null)
+                        break;
+                    sb.Append (" | ");
+                }
+                ++level;
+                yield return sb.ToString();
+                sb.Length = 0;
+            }
+
+            var leafPath = new NodeVector (this, level);
+            sb.Append ('L');
+            sb.Append (level);
+            sb.Append (": ");
+            for (var leaf = (Leaf) leftmost;;)
+            {
+                leaf.Append (sb);
+                leaf = (Leaf) leafPath.TraverseRight();
+                if (leaf == null)
+                    break;
+
+                if (leafPath.Child0)
+                    sb.Append (" | ");
+                else
+                    sb.Append ('|');
+            }
+            yield return sb.ToString();
+        }
+    }
+#endif
+}
---
+++ RankedDictionary/BtreeNode.cs
@@ -0,0 +1,213 @@
+﻿//
+// Library: KaosCollections
+// File:    BtreeNode.cs
+// Purpose: Define nonpublic tree structure and its basic operations.
+//
+// Copyright © 2009-2017 Kasey Osborn (github.com/kaosborn)
+// MIT License - Use and redistribute freely
+//
+
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Kaos.Collections
+{
+    public partial class BtreeDictionary<TKey,TValue>
+    {
+        /// <summary>A page of the B+ tree. May be internal (Branch) or terminal (Leaf).</summary>
+        private class Node
+        {
+            protected readonly List<TKey> keys;
+
+            protected Node (int keyCapacity)
+            { this.keys = new List<TKey> (keyCapacity); }
+
+            public int KeyCount { get { return keys.Count; } }
+            public TKey Key0 { get { return keys[0]; } }
+
+            public void AddKey (TKey key) { keys.Add (key); }
+            public TKey GetKey (int index) { return keys[index]; }
+            public int Search (TKey key) { return keys.BinarySearch (key); }
+            public int Search (TKey key, IComparer<TKey> comparer) { return keys.BinarySearch (key, comparer); }
+            public void SetKey (int index, TKey key) { keys[index] = key; }
+            public void RemoveKey (int index) { keys.RemoveAt (index); }
+            public void RemoveKeys (int index, int count) { keys.RemoveRange (index, count); }
+            public void TruncateKeys (int index) { keys.RemoveRange (index, keys.Count - index); }
+            public void InsertKey (int index, TKey key) { keys.Insert (index, key); }
+
+#if DEBUG
+            public StringBuilder Append (StringBuilder sb)
+            {
+                for (int ix = 0; ix < KeyCount; ix++)
+                {
+                    if (ix > 0)
+                        sb.Append (',');
+                    sb.Append (GetKey (ix));
+                }
+                return sb;
+            }
+#endif
+        }
+
+
+        /// <summary>An internal B+ tree page.</summary>
+        /// <remarks>
+        /// Contains copies of the first key of every leaf (except the leftmost).
+        /// </remarks>
+        private sealed class Branch : Node
+        {
+            private readonly List<Node> childNodes;
+
+            // Called on initialization only.
+            public Branch (Leaf leaf, int keyCapacity=0) : base (keyCapacity)
+            {
+                this.childNodes = new List<Node> (keyCapacity) { leaf };
+            }
+
+            public Branch (Branch leftBranch, int keyCapacity) : base (keyCapacity)
+            {
+                this.childNodes = new List<Node> (keyCapacity + 1);
+            }
+
+            public Branch (Node child, int keyCapacity) : base (keyCapacity)
+            {
+                this.childNodes = new List<Node> (keyCapacity + 1) { child };
+            }
+
+
+            public int ChildCount
+            { get { return childNodes.Count; } }
+
+            public Node GetChild (int childIndex)
+            { return childNodes[childIndex]; }
+
+            public Node Child0
+            { get { return childNodes[0]; } set { childNodes[0] = value; } }
+
+            public void RemoveChild (int index)
+            { childNodes.RemoveAt (index); }
+
+            public void Truncate (int index)
+            {
+                TruncateKeys (index);
+                childNodes.RemoveRange (index + 1, childNodes.Count - (index + 1));
+            }
+
+            public void Add (Node node)
+            { childNodes.Add (node); }
+
+            public void Add (TKey key, Node node)
+            {
+                AddKey (key);
+                childNodes.Add (node);
+            }
+
+            public void Insert (int index, Node node)
+            {
+                childNodes.Insert (index, node);
+            }
+
+            public void Remove (int index, int count)
+            {
+                RemoveKeys (index, count);
+                childNodes.RemoveRange (index, count);
+            }
+        }
+
+
+        /// <summary>An terminal B+ tree page.</summary>
+        /// <remarks>
+        /// All key/value pairs are contained in this class.
+        /// Leaf is a sequential linked list also referenced by parent branches.
+        /// </remarks>
+        private sealed class Leaf : Node
+        {
+            private readonly List<TValue> values;  // Payload.
+            private Leaf rightLeaf;  // For the linked leaf list.
+
+            public Leaf (int capacity=0) : base (capacity)
+            {
+                this.values = new List<TValue> (capacity);
+                this.rightLeaf = null;
+            }
+
+            /// <summary>Splice a leaf to right of <paramref name="leftLeaf"/>.</summary>
+            /// <param name="leftLeaf">Provides linked list insert point.</param>
+            /// <param name="capacity">The initial number of elements the page can store.</param>
+            public Leaf (Leaf leftLeaf, int capacity) : base (capacity)
+            {
+                this.values = new List<TValue> (capacity);
+
+                // Linked list insertion.
+                this.rightLeaf = leftLeaf.rightLeaf;
+                leftLeaf.rightLeaf = this;
+            }
+
+
+            /// <summary>Next leaf in linked list.</summary>
+            public Leaf RightLeaf
+            {
+                get { return rightLeaf; }
+                set { rightLeaf = value; }
+            }
+
+
+            public int ValueCount
+            { get { return values.Count; } }
+
+
+            public KeyValuePair<TKey,TValue> GetPair (int index)
+            { return new KeyValuePair<TKey,TValue> (keys[index], values[index]); }
+
+
+            public TValue GetValue (int index)
+            { return values[index]; }
+
+
+            public void SetValue (int index, TValue value)
+            { values[index] = value; }
+
+
+            public void Add (TKey key, TValue value)
+            {
+                AddKey (key);
+                values.Add (value);
+            }
+
+            public void Add (Leaf source, int sourceStart, int sourceStop)
+            {
+                for (int ix = sourceStart; ix < sourceStop; ++ix)
+                    Add (source.GetKey (ix), source.GetValue (ix));
+            }
+
+            public void Insert (int index, TKey key, TValue value)
+            {
+                Debug.Assert (index >= 0 && index <= ValueCount);
+                InsertKey (index, key);
+                values.Insert (index, value);
+            }
+
+            public void Remove (int index)
+            {
+                Debug.Assert (index >= 0 && index <= ValueCount);
+                RemoveKey (index);
+                values.RemoveAt (index);
+            }
+
+            public void Remove (int index, int count)
+            {
+                Debug.Assert (index >= 0 && index + count <= ValueCount);
+                RemoveKeys (index, count);
+                values.RemoveRange (index, count);
+            }
+
+            public void Truncate (int index)
+            {
+                Debug.Assert (index >= 0 && (ValueCount == 0 || index < ValueCount));
+                RemoveKeys (index, KeyCount - index);
+                values.RemoveRange (index, ValueCount - index);
+            }
+        }
+    }
+}
