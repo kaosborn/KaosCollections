3580ed5064b1d383f0da67f3308718710137b6ca
kaosborn <kaosborn@users.noreply.github.com>
2017-07-14 22:35:36 -0700

    add RankedSet class

---
+++ RankedDictionary/RankedSet.cs
@@ -0,0 +1,315 @@
+﻿//
+// Library: KaosCollections
+// File:    RankedSet.cs
+// Purpose: Defines BtreeDictionary generic API.
+//
+// Copyright © 2009-2017 Kasey Osborn (github.com/kaosborn)
+// MIT License - Use and redistribute freely
+//
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Kaos.Collections
+{
+    /// <summary>Represents a collection of key/value pairs that are sorted on the key.
+    /// </summary>
+    /// <typeparam name="TKey">The type of the keys in the dictionary.</typeparam>
+    /// <typeparam name="TValue">The type of the values in the dictionary.</typeparam>
+    /// <remarks>
+    /// This class is a functional equivalent of the
+    /// <see cref="System.Collections.Generic.SortedDictionary&lt;TKey,TValue&gt;"/>
+    /// with the addition of the methods
+    /// <see cref="BetweenKeys"/>, <see cref="SkipUntilKey"/>, and <see cref="Last"/>.
+    /// </remarks>
+    [DebuggerDisplay ("Count = {Count}")]
+    public partial class RankedSet<TKey> : Btree<TKey>,
+#if ! NET35
+        ISet<TKey>,
+#endif
+        ICollection<TKey>,
+        ICollection
+#if NETSTANDARD1_0
+        , IReadOnlyCollection<TKey>
+#endif
+        where TKey : IComparable
+    {
+        private KeyLeaf LeftmostLeaf { get { return leftmostLeaf; } }
+
+        public RankedSet (int order, IComparer<TKey> comparer) : base (order, comparer, new KeyLeaf())
+        {
+            this.root = this.leftmostLeaf;
+        }
+
+
+        public int Count
+        { get { return root.Weight; } }
+
+        bool ICollection<TKey>.IsReadOnly
+        { get { return false; } }
+
+        bool ICollection.IsSynchronized
+        { get { return false; } }
+
+        object ICollection.SyncRoot => throw new NotImplementedException ();
+
+        public void Clear()
+        {
+            leftmostLeaf.Truncate (0);
+            leftmostLeaf.rightKeyLeaf = null;
+            root = leftmostLeaf;
+        }
+
+        public bool Add (TKey item)
+        {
+            if (item == null)
+                throw new ArgumentNullException (nameof (item));
+
+            var path = new NodeVector (this, item);
+            if (path.IsFound)
+                return false;
+
+            Add2 (path, item);
+            return true;
+        }
+
+        void ICollection<TKey>.Add (TKey item)
+        { Add (item); }
+
+        private void Add2 (NodeVector nv, TKey key)
+        {
+            var leaf = (KeyLeaf) nv.TopNode;
+            int pathIndex = nv.TopNodeIndex;
+
+            nv.UpdateWeight (1);
+            if (leaf.KeyCount < maxKeyCount)
+            {
+                leaf.Insert (pathIndex, key);
+                return;
+            }
+
+            // Leaf is full so right split a new leaf.
+            var newLeaf = new KeyLeaf (leaf, maxKeyCount);
+
+            if (newLeaf.RightLeaf == null && pathIndex == leaf.KeyCount)
+                newLeaf.AddKey (key);
+            else
+            {
+                int splitIndex = leaf.KeyCount / 2 + 1;
+
+                if (pathIndex < splitIndex)
+                {
+                    // Left-side insert: Copy right side to the split leaf.
+                    newLeaf.Add (leaf, splitIndex - 1, leaf.KeyCount);
+                    leaf.Truncate (splitIndex - 1);
+                    leaf.Insert (pathIndex, key);
+                }
+                else
+                {
+                    // Right-side insert: Copy split leaf parts and new key.
+                    newLeaf.Add (leaf, splitIndex, pathIndex);
+                    newLeaf.AddKey (key);
+                    newLeaf.Add (leaf, pathIndex, leaf.KeyCount);
+                    leaf.Truncate (splitIndex);
+                }
+            }
+
+            // Promote anchor of split leaf.
+            nv.Promote (newLeaf.Key0, (Node) newLeaf, newLeaf.RightLeaf == null);
+        }
+
+
+        public bool Contains (TKey key)
+        {
+            if (key == null)
+                throw new ArgumentNullException (nameof (key));
+
+            KeyLeaf leaf = Find (key, out int index);
+            return index >= 0;
+        }
+
+        public void CopyTo (TKey[] array, int index)
+        {
+            if (array == null)
+                throw new ArgumentNullException (nameof (array));
+
+            if (index < 0)
+                throw new ArgumentOutOfRangeException (nameof (index), index, "Specified argument was out of the range of valid values.");
+
+            if (Count > array.Length - index)
+                throw new ArgumentException ("Destination array is not long enough to copy all the items in the collection. Check array index and length.", nameof (array));
+
+            for (KeyLeaf leaf = LeftmostLeaf; leaf != null; leaf = leaf.RightLeaf)
+                for (int leafIndex = 0; leafIndex < leaf.KeyCount; ++leafIndex)
+                    array[index++] = leaf.GetKey (leafIndex);
+        }
+
+        void ICollection.CopyTo (Array array, int index)
+        {
+            int ix = 0;
+            var obArray = array as object[];
+            foreach (var item in this)
+                obArray[ix++] = item;
+        }
+
+        public bool Remove (TKey key)
+        {
+            if (key == null)
+                throw new ArgumentNullException (nameof (key));
+
+            var path = new NodeVector (this, key);
+            if (! path.IsFound)
+                return false;
+
+            Remove2 (path);
+            return true;
+        }
+
+        IEnumerator<TKey> IEnumerable<TKey>.GetEnumerator()
+        {
+            return new Enumerator (this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return new Enumerator (this);
+        }
+
+        #region ISET implementation
+
+        #if ! NET35
+        public void ExceptWith (IEnumerable<TKey> other)
+        {
+            if (other == null)
+                throw new ArgumentNullException (nameof(other));
+
+            if (Count == 0)
+                return;
+
+            if (other == this)
+            {
+                Clear();
+                return;
+            }
+
+            SortedSet<TKey> other2 = other as SortedSet<TKey>;
+
+            if (other2 == null)
+            {
+                Clear();
+                return;
+            }
+
+            throw new NotImplementedException();
+        }
+
+        public void IntersectWith (IEnumerable<TKey> other)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public bool IsProperSubsetOf (IEnumerable<TKey> other)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public bool IsProperSupersetOf (IEnumerable<TKey> other)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public bool IsSubsetOf (IEnumerable<TKey> other)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public bool IsSupersetOf (IEnumerable<TKey> other)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public bool Overlaps (IEnumerable<TKey> other)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public bool SetEquals (IEnumerable<TKey> other)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public void SymmetricExceptWith (IEnumerable<TKey> other)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public void UnionWith (IEnumerable<TKey> other)
+        {
+            throw new NotImplementedException ();
+        }
+
+#endif
+        #endregion
+
+        /// <summary>Enumerates the sorted elements of a KeyCollection.</summary>
+        public sealed class Enumerator : IEnumerator<TKey>
+        {
+            private readonly RankedSet<TKey> tree;
+            private KeyLeaf currentLeaf;
+            private int leafIndex;
+
+            internal Enumerator (RankedSet<TKey> set)
+            {
+                this.tree = set;
+                ((IEnumerator) this).Reset();
+            }
+
+            object IEnumerator.Current
+            {
+                get
+                {
+                    if (leafIndex < 0)
+                        throw new InvalidOperationException();
+                    return (object) Current;
+                }
+            }
+
+            /// <summary>
+            /// Gets the element at the current position of the enumerator.
+            /// </summary>
+            public TKey Current
+            { get { return leafIndex < 0? default (TKey) : currentLeaf.GetKey (leafIndex); } }
+
+            /// <summary>Advances the enumerator to the next element in the collection.</summary>
+            /// <returns><b>true</b> if the enumerator was successfully advanced to the next element; <b>false</b> if the enumerator has passed the end of the collection.</returns>
+            public bool MoveNext()
+            {
+                if (currentLeaf != null)
+                {
+                    if (++leafIndex < currentLeaf.KeyCount)
+                        return true;
+
+                    currentLeaf = currentLeaf.RightLeaf;
+                    if (currentLeaf != null)
+                    { leafIndex = 0; return true; }
+
+                    leafIndex = -1;
+                }
+
+                return false;
+            }
+
+            void IEnumerator.Reset()
+            {
+                leafIndex = -1;
+                currentLeaf = tree.LeftmostLeaf;
+            }
+
+            /// <summary>Releases all resources used by the Enumerator.</summary>
+            public void Dispose() { }
+        }
+
+    }
+}
