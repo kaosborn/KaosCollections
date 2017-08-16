040f50fd13f7e995efac34c03af44c07e2f63dae
kaosborn <kaosborn@users.noreply.github.com>
2017-08-16 00:45:42 -0700

    add Collections462, Test462 projects

---
+++ Collections462/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle ("Collections462")]
+[assembly: AssemblyDescription ("for test only")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct ("KaosCollections")]
+[assembly: AssemblyCopyright ("Copyright © 2017 github.com/kaosborn")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+[assembly: ComVisible (false)]
+[assembly: Guid ("c385faa0-382e-46e6-9557-3e1760edaab0")]
+[assembly: AssemblyVersion ("0.1.0.0")]
