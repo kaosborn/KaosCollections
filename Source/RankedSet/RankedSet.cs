bfe69ef59d158daca2be3aba28c94cd9cf13074d
kaosborn <kaosborn@users.noreply.github.com>
2017-08-18 19:03:43 -0700

    add internal AddKey method

--- Source/RankedSet/RankedSet.cs
+++ Source/RankedSet/RankedSet.cs
@@ -140,55 +140,7 @@ namespace Kaos.Collections
             if (path.IsFound)
                 return false;
 
-            StageBump();
-
-            path.IncrementPathWeight();
-
-            var leaf = (Leaf) path.TopNode;
-            if (leaf.KeyCount < maxKeyCount)
-            {
-                leaf.Insert (path.TopIndex, item);
-                return true;
-            }
-
-            // Leaf is full so right split a new leaf.
-            var newLeaf = new Leaf (leaf, maxKeyCount);
-            int pathIndex = path.TopIndex;
-
-            if (newLeaf.rightLeaf != null)
-                newLeaf.rightLeaf.leftLeaf = newLeaf;
-            else
-            {
-                rightmostLeaf = newLeaf;
-
-                if (pathIndex == leaf.KeyCount)
-                {
-                    newLeaf.AddKey (item);
-                    path.Promote (item, (Node) newLeaf, true);
-                    return true;
-                }
-            }
-
-            int splitIndex = leaf.KeyCount / 2 + 1;
-            if (pathIndex < splitIndex)
-            {
-                // Left-side insert: Copy right side to the split leaf.
-                newLeaf.Add (leaf, splitIndex - 1, leaf.KeyCount);
-                leaf.Truncate (splitIndex - 1);
-                leaf.Insert (pathIndex, item);
-            }
-            else
-            {
-                // Right-side insert: Copy split leaf parts and new key.
-                newLeaf.Add (leaf, splitIndex, pathIndex);
-                newLeaf.AddKey (item);
-                newLeaf.Add (leaf, pathIndex, leaf.KeyCount);
-                leaf.Truncate (splitIndex);
-            }
-
-            // Promote anchor of split leaf.
-            path.Promote (newLeaf.Key0, (Node) newLeaf, newLeaf.rightLeaf == null);
-            return true;
+            return AddKey (item, path);
         }
 
 
