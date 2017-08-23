d0acf0943c25781b2f22e8f797f29f637dd3d9eb
kaosborn <kaosborn@users.noreply.github.com>
2017-08-23 00:58:29 -0700

    add RankedCollection project

---
+++ Source/RankedCollection/Btree-Collection.cs
@@ -0,0 +1,19 @@
+﻿//
+// Library: KaosCollections
+// File:    RankedCollection.cs
+//
+// Copyright © 2009-2017 Kasey Osborn (github.com/kaosborn)
+// MIT License - Use and redistribute freely
+//
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Kaos.Collections
+{
+    public abstract partial class Btree<T>
+    {
+    }
+}
