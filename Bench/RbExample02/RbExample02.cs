8d7013d428d69421a6ef96865fa73e668d192cb6
kaosborn <kaosborn@users.noreply.github.com>
2017-08-24 19:53:55 -0700

    add RbExample02, RbExample03 projects

---
+++ Bench/RbExample02/RbExample02.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Kaos.Collections;
+
+namespace ExampleApp
+{
+    public class PersonAgeComparer : System.Collections.Generic.Comparer<Person>
+    {
+        public override int Compare (Person x, Person y) =>
+            x.Age != y.Age ? x.Age - y.Age : String.Compare (x.Name, y.Name);
+    }
+
+    public class Person
+    {
+        public String Name { get; private set; }
+        public int Age { get; private set; }
+        public Person (string name, int age)
+        { this.Name = name; this.Age = age; }
+        public override string ToString () => "[ " + Age + ", " + Name + " ]";
+    }
+
+
+    class RbExample02
+    {
+        static float Mean (RankedBag<Person> rs)
+        {
+            float sum = 0;
+            foreach (var p in rs) sum += p.Age;
+            return sum / rs.Count;
+        }
+
+        static int Median (RankedBag<Person> rs)
+        {
+            return rs.ElementAt(rs.Count/2).Age;
+        }
+
+        static void Main()
+        {
+            var persons = new Person[]
+            { new Person ("Jimi Hendrix", 27), new Person ("Amy Winehouse", 27),
+            new Person ("Janis Joplin", 27), new Person ("Jim Morrison", 27),
+            new Person ("Kurt Cobain", 27), new Person ("John Lennon", 40),
+            new Person ("Keith Moon", 32), new Person ("Randy Rhoads", 25) };
+
+            var rs = new RankedBag<Person> (persons, new PersonAgeComparer());
+
+            Console.WriteLine ("Dead rock stars:");
+            foreach (var x1 in rs)
+                Console.WriteLine (x1);
+
+            var seekMoon = new Person ("Keith aMoonx", 32);
+
+            var moonIx = rs.IndexOf (seekMoon);
+            Console.WriteLine ("moonIx = " + moonIx + ", ~moonIx = " + ~moonIx);
+
+            Console.WriteLine ("\nMedian age of death: " + Median (rs));
+            Console.WriteLine ("Average age of death: " + Mean (rs));
+
+            Console.WriteLine ("Died at 27:");
+            foreach (var p in rs.ElementsBetween (new Person ("", 26), new Person ("zz", 27)))
+                Console.WriteLine ("  " + p);
+        }
+    }
+}
---
+++ Bench/RbExample03/RbExample03.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using Kaos.Collections;
+
+namespace ExampleApp
+{
+    class RbExample03
+    {
+        static double Median (RankedBag<int> vals)
+        {
+            if (vals.Count == 0) return 0D;
+            if (vals.Count % 2 == 1) return (double) vals.ElementAt (vals.Count/2);
+            return (vals.ElementAt (vals.Count/2-1) + vals.ElementAt (vals.Count/2)) / 2D;
+        }
+
+        static double Mean (RankedBag<int> vals) => vals.Sum() / (double) vals.Count;
+        static double Variance (RankedBag<int> vals) => vals.Sum (x => Math.Pow (Mean (vals) - x, 2)) / vals.Count;
+        static double StandardDeviation (RankedBag<int> vals) => Math.Sqrt (Variance (vals));
+
+        static void Main()
+        {
+            var scores = new RankedBag<int> (new int[] { 2, 5, 4, 4, 5, 4, 7, 9 });
+
+            var mean = Mean (scores);
+            var stddev = StandardDeviation (scores);
+
+            Console.WriteLine ("Count = " + scores.Count + ", median = " + Median (scores) + ", mean = " + Mean (scores));
+            Console.WriteLine ("Variance = " + Variance (scores));
+            Console.WriteLine ("Standard deviation = " + StandardDeviation (scores));
+
+            Console.WriteLine ("\nLow score: " + scores.Min);
+            Console.WriteLine ("High score: " + scores.Max);
+
+            Console.Write ("Scores within 1 standard deviation:");
+            foreach (var score in scores.ElementsBetween ((int) (mean-stddev+0.5), (int) (mean+stddev+0.5)))
+                Console.Write (" " + score);
+            Console.WriteLine();
+        }
+    }
+}
