079513c32032c7e92d1332054b7498c87cbb4a30
kaosborn <kaosborn@users.noreply.github.com>
2017-10-08 18:21:42 -0700

    replace multimap example

--- Bench/RbExample02/RbExample02.cs
+++ Bench/RbExample02/RbExample02.cs
@@ -3,71 +3,40 @@ using Kaos.Collections;
 
 namespace ExampleApp
 {
-    public class PersonAgeComparer : System.Collections.Generic.Comparer<Person>
+    public class CharMap : IComparable<CharMap>
     {
-        public override int Compare (Person x, Person y) =>
-            x.Age != y.Age ? x.Age - y.Age : String.Compare (x.Name, y.Name);
+        public char Ch { get; private set; }
+        public int Pos { get; private set; }
+        public CharMap (char ch, int pos) { Ch = ch; Pos = pos; }
+        public int CompareTo (CharMap other) => Ch.CompareTo (other.Ch);
+        public override string ToString() => Ch + " :: " + Pos;
     }
-
-    public class Person
-    {
-        public String Name { get; private set; }
-        public int Age { get; private set; }
-
-        public Person (string name, int age)
-        { this.Name = name; this.Age = age; }
-
-        public override string ToString() => "[ " + Age + ", " + Name + " ]";
-    }
-
 
     class RbExample02
     {
-        static int Median (RankedBag<Person> rs) => rs.ElementAt (rs.Count/2).Age;
-
         static void Main()
         {
-            var persons = new Person[]
-            {
-                new Person ("Jimi Hendrix", 27), new Person ("Amy Winehouse", 27),
-                new Person ("Janis Joplin", 27), new Person ("Jim Morrison", 27),
-                new Person ("Kurt Cobain", 27), new Person ("John Lennon", 40),
-                new Person ("Keith Moon", 32), new Person ("Randy Rhoads", 25)
-            };
+            var map = new RankedBag<CharMap>();
+            string s1 = "this is it";
 
-            var rockStars = new RankedBag<Person> (persons, new PersonAgeComparer());
+            for (int pos = 0; pos < s1.Length; ++pos)
+                if (! Char.IsWhiteSpace (s1[pos]))
+                    map.Add (new CharMap (s1[pos], pos));
 
-            Console.WriteLine ("Dead rock stars:");
-            foreach (var rs in rockStars)
-                Console.WriteLine ("  " + rs);
-
-            Console.WriteLine ("\nMedian age of death: " + Median (rockStars));
-
-            Console.WriteLine ("\nDied at 27:");
-            foreach (var rs27 in rockStars.ElementsBetween (new Person ("", 26), new Person ("zz", 27)))
-                Console.WriteLine ("  " + rs27);
+            foreach (var mapItem in map)
+                Console.WriteLine (mapItem);
         }
 
         /* Output:
 
-        Dead rock stars:
-          [ 25, Randy Rhoads ]
-          [ 27, Amy Winehouse ]
-          [ 27, Janis Joplin ]
-          [ 27, Jim Morrison ]
-          [ 27, Jimi Hendrix ]
-          [ 27, Kurt Cobain ]
-          [ 32, Keith Moon ]
-          [ 40, John Lennon ]
-
-        Median age of death: 27
-
-        Died at 27:
-          [ 27, Amy Winehouse ]
-          [ 27, Janis Joplin ]
-          [ 27, Jim Morrison ]
-          [ 27, Jimi Hendrix ]
-          [ 27, Kurt Cobain ]
+        h :: 1
+        i :: 2
+        i :: 5
+        i :: 8
+        s :: 3
+        s :: 6
+        t :: 0
+        t :: 9
 
         */
     }
