111359170b9b5fdff52fafd79a07b576a3faba33
kaosborn <kaosborn@users.noreply.github.com>
2017-10-09 20:03:06 -0700

    edit test

--- Test462/TestRb.cs
+++ Test462/TestRb.cs
@@ -854,7 +854,7 @@ namespace Kaos.Test.Collections
             var bag1 = new RankedBag<int>() { Capacity = 4 };
             var bag2 = new RankedBag<int>() { 3, 5, 7 };
 
-            foreach (int ii in new int[] { 3, 4, 5, 5, 6, 6, 7, 7, 8 })
+            foreach (int ii in new int[] { 3, 4, 5, 5, 6, 6, 6, 7, 7, 8 })
                 bag1.Add (ii);
 
             int del0 = bag0.RetainAll (new int[] { 2, 4 });
@@ -862,7 +862,7 @@ namespace Kaos.Test.Collections
             Assert.AreEqual (0, bag0.Count);
 
             int del1 = bag1.RetainAll (new int[] { 1, 4, 4, 6, 6, 9 });
-            Assert.AreEqual (6, del1);
+            Assert.AreEqual (7, del1);
             Assert.IsTrue (System.Linq.Enumerable.SequenceEqual (new int[] { 4, 6, 6 }, bag1));
 
             int del2 = bag2.RetainAll (new int[] { });
