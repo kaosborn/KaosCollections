3cb1c477c5bab4e87b1f4ceb22802243afc6e538
kaosborn <kaosborn@users.noreply.github.com>
2017-10-29 20:15:34 -0700

    rev to 3.0.1

--- Collections461/Properties/AssemblyInfo.cs
+++ Collections461/Properties/AssemblyInfo.cs
@@ -11,4 +11,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture ("")]
 [assembly: ComVisible (false)]
 [assembly: Guid ("c385faa0-382e-46e6-9557-3e1760edaab0")]
-[assembly: AssemblyVersion ("3.0.0.0")]
+[assembly: AssemblyVersion ("3.0.1.0")]
