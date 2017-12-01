a002bbd1df397ca00347010da340e7ab11c6712d
kaosborn <kaosborn@users.noreply.github.com>
2017-12-01 00:29:51 -0800

    edit RsBench01 for BCL comparison

--- Bench/RsBench01/RsBench01.cs
+++ Bench/RsBench01/RsBench01.cs
@@ -1,12 +1,14 @@
 ﻿//
 // Program: RsBench01.cs
-// Purpose: Benchmark old RemoveWhere (v3.1) versus new RemoveWhere (v4.0).
+// Purpose: Benchmark SortedSet.RemoveWhere versus RankedSet.RemoveWhere (v4).
 //
 // Usage notes:
-// • Without a special development configuration this test is not reproducible.
+// • Adjust 'maxN' to change test duration.
+// • For valid time results, run Release build outside Visual Studio.
 //
 
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using Kaos.Collections;
 [assembly: System.Reflection.AssemblyVersion ("0.1.0.0")]
@@ -15,28 +17,27 @@ namespace BenchApp
 {
     class RsBench01
     {
-        static int division, divisionCount = 10;
+        static int division = 1, divisionCount = 10;
         static bool IsCull (int x) => x % divisionCount < division;
 
         static void Main()
         {
-            int n = 50000000;
+            int maxN = 500000;
             var watch = new Stopwatch();
+            bool isPass1 = true;
 
-            Console.WriteLine ("n;Removals;RankedBagMs;RankedSetMs");
+            Console.WriteLine ("Removals in Thousands,Set Size in Thousands,RankedSet,SortedSet");
 
-            for (division = 0; division <= divisionCount; ++division)
+            for (int n = maxN/10; n <= maxN;)
             {
-                // RankedBag is using the old algorithm:
-                var c1 = new RankedBag<int>();
+                var c1 = new RankedSet<int>();
                 for (int i = 0; i < n; ++i) c1.Add (i);
                 watch.Reset(); watch.Start();
                 c1.RemoveWhere (IsCull);
                 var c1Time = watch.ElapsedMilliseconds;
                 int c1Count = c1.Count; c1.Clear();
 
-                // RankedSet is using the new algorithm:
-                var c2 = new RankedSet<int>();
+                var c2 = new SortedSet<int>();
                 for (int i = 0; i < n; ++i) c2.Add (i);
                 watch.Reset(); watch.Start();
                 c2.RemoveWhere (IsCull);
@@ -44,25 +45,32 @@ namespace BenchApp
                 int c2Count = c2.Count; c2.Clear();
 
                 int removes = n * division / divisionCount;
-                Console.WriteLine ("{0},{1},{2},{3}", n, removes, c1Time, c2Time);
+                if (c1Count != n-removes || c2Count != n-removes)
+                    Console.WriteLine ("*** ERROR ***" + c1Count + ", " + c2Count);
+
+                if (isPass1)
+                    isPass1 = false;
+                else
+                {
+                    Console.WriteLine ("{0},{1},{2},{3}", removes/1000, n/1000, c1Time, c2Time);
+                    n += maxN/10;
+                }
             }
         }
 
+        /* Output:
 
-        /* Output for old RemoveWhere (RankedBag) vs. new RemoveWhere (RankedSet):
-
-        n;Removals;RankedBagMs;RankedSetMs
-        50000000,0,2999,3908
-        50000000,5000000,18050,4183
-        50000000,10000000,32200,4142
-        50000000,15000000,47197,4172
-        50000000,20000000,60528,4105
-        50000000,25000000,76493,6725
-        50000000,30000000,87890,6391
-        50000000,35000000,106109,6483
-        50000000,40000000,115870,7819
-        50000000,45000000,133775,7950
-        50000000,50000000,121599,5761
+        Removals in Thousands,Set Size in Thousands,RankedSet,SortedSet
+        5,50,5,1068
+        10,100,8,5264
+        15,150,13,12531
+        20,200,16,21113
+        25,250,20,23936
+        30,300,24,51612
+        35,350,28,59628
+        40,400,32,85661
+        45,450,36,91218
+        50,500,40,107109
 
         */
     }
