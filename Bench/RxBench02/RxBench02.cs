4cb65d6f6ace71e4a639b479f2c828215112a32f
kaosborn <kaosborn@users.noreply.github.com>
2018-03-25 22:50:18 -0700

    show BCL Dispose curiosity

--- Bench/RxBench02/RxBench02.cs
+++ Bench/RxBench02/RxBench02.cs
@@ -1,8 +1,11 @@
 ﻿//
 // Program: RxBench02.cs
-// Purpose: Shows library source compiled into the same assembly as the executable.
+// Purpose:
+// • Exercise library source compiled into the same assembly as the executable.
+// • Demonstrate BCL curiosities.
 //
 
+using System;
 using System.Collections.Generic;
 using Kaos.Collections;
 [assembly: System.Reflection.AssemblyVersion ("0.1.0.0")]
@@ -13,20 +16,36 @@ namespace BenchApp
     {
         static void Main()
         {
-            var rs = new RankedSet<int>();
-
             // Here is an example of why 'private protected' is better than 'internal'.
             // Kaos.Collections v4.1 and earlier it was internal and would have compiled for RxBench02.
             // After v4.1, as private protected it is not accessible.
 
-            // No longer compiles for single-assembly projects:
-            // var kc = rs.maxKeyCount;
-
-            // Asides:
+            // Commented out assignment no longer compiles for single-assembly projects:
+            var rs = new RankedSet<int>();
+         // var kc = rs.maxKeyCount;
 
             // Here is a curiosity where the BCL allows creation of an enumerator without a target class.
             // This default constructor is not emulated by Kaos.Collections.
             var strangeEnumerator = new SortedSet<int>.Enumerator();
+
+            // Another curiosity: BCL enumerators remain active after disposing.
+            // The KaosCollections library emulates this seemingly wrong behavior.
+
+            var ss = new SortedSet<int> { 3,4,5 };
+            SortedSet<int>.Enumerator ssEtor = ss.GetEnumerator();
+            bool ssNext1 = ssEtor.MoveNext();
+            ssEtor.Dispose();
+            bool ssNext2 = ssEtor.MoveNext();
+
+            Console.WriteLine ("Dispose() then MoveNext() = " + ssNext2);
+            Console.WriteLine ("Dispose() then Current = " + ssEtor.Current);
         }
+
+        /* Output:
+
+        Dispose() then MoveNext() = True
+        Dispose() then Current = 4
+
+        */
     }
 }
