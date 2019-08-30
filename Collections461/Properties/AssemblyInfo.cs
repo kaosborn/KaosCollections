70f23d01137e8c303feaeb263d4f553da4559bff
kaosborn <kaosborn@users.noreply.github.com>
2019-08-29 22:46:47 -0700

    rev to 4.2.0.1

--- Collections461/Properties/AssemblyInfo.cs
+++ Collections461/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle ("KaosCollections")]
-[assembly: AssemblyDescription ("for test and documentation only")]
+[assembly: AssemblyDescription ("for test and development only")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
 [assembly: AssemblyProduct ("KaosCollections")]
@@ -11,4 +11,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture ("")]
 [assembly: ComVisible (false)]
 [assembly: Guid ("c385faa0-382e-46e6-9557-3e1760edaab0")]
-[assembly: AssemblyVersion ("4.2.0.0")]
+[assembly: AssemblyVersion ("4.2.0.1")]
